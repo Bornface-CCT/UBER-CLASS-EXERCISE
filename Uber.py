8e9bbb428b971831980f3d0a17dbac479f067ed5
Bornface-CCT <sba25370@student.cct.ie>
2026-05-12 18:48:53 +0100

    update

---
+++ Uber.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+st.title("Uber Pickups in New York")
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
